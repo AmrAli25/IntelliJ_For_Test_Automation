fb6bc001be230d7a8ff3461f82765a3406eeea57
Amr Ali <amraliahmed25@gmail.com>
2023-12-01 18:34:57 +0200

    Add test case ch6 in TAU IntelliJ for Test Automation

--- src/test/java/taudemo/SelenuimTest.java
+++ src/test/java/taudemo/SelenuimTest.java
@@ -1,4 +1,36 @@
 package taudemo;
 
+import browser.BrowserGetter;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class SelenuimTest {
+
+    private final BrowserGetter getter = new BrowserGetter();
+    private WebDriver driver;
+
+    @BeforeAll
+    public void beforeAll() {
+        driver = getter.getChromeDriver();
+
+    }
+
+    @AfterAll
+    public void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void openThePageAndCheckTheTitle() {
+        String expectedTitle = "Example Title";
+        driver.get("https://www.example.com");
+        assertEquals(expectedTitle, driver.getTitle());
+    }
+
+
 }
